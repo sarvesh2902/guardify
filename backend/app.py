ed1dc1b313b99e01243bcb626f4c1842ce039495
Sarvesh Patil <sarvesh2902@gmail.com>
2023-03-13 11:55:15 +0530

    changed requirements.txt and updated env variables

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
-from bs4 import BeautifulSoup
-import argparse
-import configparser
-import pandas as pd
-import time
-from tweepy import API, Cursor, OAuthHandler, TweepyException
-import numpy as np
-import csv
+from tweepy import API, Cursor, OAuthHandler
 from flask_cors import CORS
 from googletrans import Translator
 import openai
@@ -19,48 +12,12 @@ CORS(app)
 translator = Translator()
 
 
-token = "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"
-consumer_key = "c3sFnv7hxrfgRk5zN4VT1tfpk"
-consumer_secret = "Tmr7cq2PLOUEA8nP2zrW5w5OT2yKieu5HE4FuOniyEZfin9Ktv"
-access_token = "1390587786491617282-5K8Sz2lHtG35RP1UzjHnkl1LvvkYyw"
-access_secret = "4CGdhXjBIlELdJqlm0vXdyQoDtslmVaNmgMebqSSs1ADf"
+# token = "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"
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+
+auth = OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_SECRET"))
 api = API(auth, wait_on_rate_limit=True)
-
-
-# @app.route("/analyzer", methods=["POST"])
-# def hello():
-
-#     session = HTMLSession()
-#     url = "https://twitter.com/user/status/1633845058675027970"
-
-#     r = session.get(url)
-#     r.html.render(sleep=2)
-
-#     tweet_text = r.html.find(".css-1dbjc4n.r-1s2bzr4", first=True)
-
-#     print(tweet_text.text)
-
-
-# @app.route("/scrape", methods=["POST"])
-# def scrape1():
-#     r = requests.get("https://twitter.com/user/status/1633845058675027970")
-#     soup = BeautifulSoup(r.text, "html.parser")
-
-#     category = []
-#     size = []
-#     price = []
-#     floor = []
-#     for item in soup.findAll(
-#         "span",
-#         {"class": "css-901oao css-16my406 css-1hf3ou5 r-poiln3 r-bcqeeo r-qvutc0"},
-#     ):
-#         category.append(item.get_text(strip=True))
-
-#     print(category)
-#     return "ok"
 
 
 @app.route("/get-followings", methods=["POST"])
